835781509f10b090d67d963fbd232fe4bd5ca863
royanish123 <anish.anandroy@gmail.com>
2025-12-25 19:45:12 +0530

    Added framer motion animation

--- src/components/nyan-cat.tsx
+++ src/components/nyan-cat.tsx
@@ -7,6 +7,7 @@ import {
   animate,
   useAnimationControls,
 } from "framer-motion";
+import Image from "next/image";
 
 const getRandomHeight = () => {
   return `${Math.random() * 100}vh`;
@@ -91,7 +92,7 @@ const AnimatedDiv = ({
       onAnimationComplete={onCompleted}
       onClick={handlePause}
     >
-      <img
+      <Image
         src="/assets/nyan-cat.gif"
         className={cn("fixed z-10 h-40 w-auto")}
         alt="Nyan Cat"
